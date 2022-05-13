7ce4f29e8b94305bf120135e9f49d9df9b9ee657
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-13 16:55:17 -0700

    setting list state

--- src/views/ShoppingList.jsx
+++ src/views/ShoppingList.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react';
 
+const initialItems = [
+  { id: 1, name: 'happiness', bought: false },
+  { id: 2, name: 'meaning to life', bought: false },
+];
+
+const listReducer = (state, action) => {
+
+};
 
 export default function ShoppingList() {
+  const [items, dispatch] = useReducer(listReducer, initialItems);
+  const [newItem, setNewItem] = useState('');
+
   return (
-    <div>ShoppingList</div>
+    <h1>Shopping List:</h1>
   )
   // form/input
   // map items
