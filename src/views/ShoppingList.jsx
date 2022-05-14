c62d62084e1b17096738ae80cd6f479ee0101f74
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-13 17:06:36 -0700

    added list form

--- src/views/ShoppingList.jsx
+++ src/views/ShoppingList.jsx
@@ -12,9 +12,28 @@ const listReducer = (state, action) => {
 export default function ShoppingList() {
   const [items, dispatch] = useReducer(listReducer, initialItems);
   const [newItem, setNewItem] = useState('');
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    dispatch({ type: 'ADD_ITEM', payload: { name: newItem } });
+  }
 
   return (
-    <h1>Shopping List:</h1>
+    <>
+      <h1>Shopping List:</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type='text'
+          name='newItem'
+          placeholder='add item'
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          required
+        />
+        <button>Add item</button>
+      </form>
+    </>
   )
   // form/input
   // map items
